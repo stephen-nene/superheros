eb275591e2d4f5fb096c830cbdc6cbc005116c37
stephen-nene <stephen.nene@student.moringaschool.com>
2023-03-27 08:59:16 +0300

    new seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# Delete all existing data
+HeroPower.delete_all
+Power.delete_all
+Hero.delete_all
+
+# Create powers
+powers = []
+10.times do
+  powers << Power.create(
+    name: Faker::Superhero.power,
+    description: Faker::ChuckNorris.fact
+  )
+end
+
+# Create heroes
+30.times do
+  hero = Hero.create(
+    name: Faker::Name.name,
+    super_name: Faker::Superhero.name
+  )
+
+  # Assign random powers to hero
+  powers.sample(rand(1..3)).each do |power|
+    HeroPower.create(hero: hero, power: power)
+  end
+end
